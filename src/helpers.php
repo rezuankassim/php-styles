b98952e499e018ef79c9f33306ef54b72833e1a8
Rezuan Kassim <rezuankassim@hotmail.com>
2020-10-20 14:30:21 +0800

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -6,20 +6,7 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder; 
 
 function styles(Finder $finder, array $rules = []): Config {
-    $project_path = getcwd();
-    $finder = $finder
-        ->in([
-            $project_path . '/app',
-            $project_path . '/config',
-            $project_path . '/database',
-            $project_path . '/resources',
-            $project_path . '/routes',
-            $project_path . '/tests',
-        ])
-        ->name('*.php')
-        ->notName('*.blade.php')
-        ->ignoreDotFiles(true)
-        ->ignoreVCS(true);
+    $rules = array_merge(require __DIR__.'/rules.php', $rules);
     
     return Config::create()
         ->setFinder($finder)
